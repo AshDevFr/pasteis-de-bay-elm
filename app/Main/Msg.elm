2ac5dbc0fbfce3b00581f08a77a4573f2b40ae5f
Franck Verrot <franck@verrot.fr>
2017-11-27 20:18:21 -0800

    Simplify constants

--- app/Main/Msg.elm
+++ app/Main/Msg.elm
@@ -1,17 +1,17 @@
 module Main.Msg exposing (..)
 
-import Business.Msg exposing (Msg)
-import Manufacturing.Msg exposing (Msg)
-import Projects.Msg exposing (Msg)
+import Business.Msg as Business exposing (Msg)
+import Manufacturing.Msg as Manufacturing exposing (Msg)
+import Projects.Msg as Projects exposing (Msg)
 import Projects.Model exposing (Project)
 import Cheats.Model exposing (CheatModel)
 import Time exposing (Time)
 
 
 type Msg
-    = BusinessMessage Business.Msg.Msg
-    | ManufacturingMessage Manufacturing.Msg.Msg
-    | ProjectsMessage Projects.Msg.Msg
+    = BusinessMessage Business.Msg
+    | ManufacturingMessage Manufacturing.Msg
+    | ProjectsMessage Projects.Msg
     | Tick Time
     | UpdateModel
     | BuyPasteis
