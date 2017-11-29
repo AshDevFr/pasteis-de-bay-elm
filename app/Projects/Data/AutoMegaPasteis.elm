9488c66c442311b7159a4edbfe31ce708f47f162
Sylvain CAU <ashdevfr@gmail.com>
2017-11-28 17:48:25 -0800

    :wrench:

--- app/Projects/Data/AutoMegaPasteis.elm
+++ app/Projects/Data/AutoMegaPasteis.elm
@@ -3,7 +3,7 @@ module Projects.Data.AutoMegaPasteis exposing (allProjects)
 import Projects.Module.ProjectsModule exposing (ProjectsModule)
 import Projects.Model exposing (Project, ProjectCost)
 import Projects.Msg exposing (Msg(..))
-import Projects.Data.Utils exposing (..)
+import Projects.Data.Utils as Utils
 
 
 allProjects : List Project
